7020f1239e80330f6980578a70d8099288a826c6
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-09-22 01:51:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const bot = require(__dirname + '/lib/amd')
+const { VERSION } = require(__dirname + '/config')
+
+const start = async () => {
+    Debug.info(`Starting wasi ${VERSION}`)
+  try {
+    await bot.init()
+    //bot.logger.info('⏳ Database syncing!')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
+/**
+
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓███████▓▒░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░░▒▓██████▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░ 
+ ░▒▓█████████████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░ 
+                                                      
+
+                                                      
+**/
